4c862121946931fe2ba0bdcbedcc6bc6d55fce4e
haraamurti <haramurti@student.ub.ac.id>
2025-09-09 00:03:13 +0700

    sixth commit adding struct to the basic learning

--- 9-struct/struct.go
+++ 9-struct/struct.go
@@ -7,10 +7,29 @@ type film struct {
 	director string
 	sound string
 	duration int
-
-
 }
 
 func main (){
 	fmt.Println("hello world ")
+	
+	var film1 film
+	var film2 film
+
+
+	//setting untuk film1
+	film1.title = "hollow moving's castle"
+	film1.director = "ghibli studio"
+	film1.duration = 1
+	film1.sound = "joe hisashi"
+
+	//setting untuk film2
+	film2.title = "the wind Rises"
+	film2.director = "ghibli studio"
+	film2.duration = 2
+	film2.sound = "joe hisashi"
+
+
+	fmt.Println("Film title : " + film1.title)
+	//how to access to func main
+
 }
