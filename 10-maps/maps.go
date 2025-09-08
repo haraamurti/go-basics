cf30ce729f4e8a09d66570705b3db83a6910a0fb
haraamurti <haramurti@student.ub.ac.id>
2025-09-09 00:07:54 +0700

    seventh commit adding maps

---
+++ 10-maps/maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	a := map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+
+
+}
+
