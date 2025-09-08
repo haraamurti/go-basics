cbd582d01800fae69b9951534a265c9dff8812fb
haraamurti <haramurti@student.ub.ac.id>
2025-09-08 23:48:57 +0700

    fourth commit adding function.go

--- 8-functions/functions.go
+++ 8-functions/functions.go
@@ -13,13 +13,20 @@ func withparameter(name string ){
 }
 
 //with return value
-func with_return_value(){
+func with_return_value(nama string )string {
+	return nama + "anjay"
+}
 
-
+//storing return value
+func stroing_value_in_var (x int ) (result int){
+	result = 5*x
+	return
 }
 
 func main (){
 	fmt.Println("hello world")
 	im_the_best_function()
 	withparameter("neval")
+	with_return_value("ijal")
+	fmt.Println(stroing_value_in_var(5))
 }
